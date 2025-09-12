62f9b3ade42099f72af04169e5346ffc951719ab
jefersoncostabr <jefersonb88@gmail.com>
2025-09-12 11:16:42 -0300

    correção de nome na linha 10 routes

--- routes/routes.js
+++ routes/routes.js
@@ -7,7 +7,7 @@ router.get("/", (req, res) => {
     res.status(200).send("Rota inicial funcionando");
 });
 
-router.use("/pr", express.static("frontEnd/painelGeral"));// http://localhost:3000/painel/painelGeral.html
+router.use("/pr", express.static("frontend/painelGeral"));// http://localhost:3000/painel/painelGeral.html
 router.use("/usos", routerUsos);// http://localhost:3000/usos
 
 export default router;
