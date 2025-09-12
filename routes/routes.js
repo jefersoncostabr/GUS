18b4ce40fadc5bf5f9ee59ddcabf4fe480c6db8e
jefersoncostabr <jefersonb88@gmail.com>
2025-09-11 22:46:30 -0300

    teste dirname

--- routes/routes.js
+++ routes/routes.js
@@ -4,7 +4,8 @@ import path from 'path';
 
 const router = express.Router();
 
-router.use("/painel", express.static(path.join(process.cwd(), "frontEnd/painelGeral")));// http://localhost:3000/painel/painelGeral.html
+// router.use("/painel", express.static(path.join(process.cwd(), "frontEnd/painelGeral")));// http://localhost:3000/painel/painelGeral.html
+router.use("/painel", express.static(path.join(__dirname, '../frontend')));// http://localhost:3000/painel/painelGeral.html
 router.use("/usos", routerUsos);// http://localhost:3000/usos
 
 export default router;
