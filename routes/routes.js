1b1ac361f6c50995f226f2a770ca51f54720ff9a
jefersoncostabr <jefersonb88@gmail.com>
2025-09-12 11:05:31 -0300

    rota barra como teste

--- routes/routes.js
+++ routes/routes.js
@@ -1,11 +1,13 @@
 import express from 'express';
 import routerUsos from './routerUsos.js';
-import path from 'path';
 
 const router = express.Router();
 
-// router.use("/painel", express.static(path.join(process.cwd(), "frontEnd/painelGeral")));// http://localhost:3000/painel/painelGeral.html
-router.use("/painel", express.static(path.join(__dirname, '../frontend')));// http://localhost:3000/painel/painelGeral.html
+router.get("/", (req, res) => {
+    res.status(200).send("Rota inicial funcionando");
+});
+
+router.use("/pr", express.static("frontEnd/painelGeral"));// http://localhost:3000/painel/painelGeral.html
 router.use("/usos", routerUsos);// http://localhost:3000/usos
 
 export default router;
