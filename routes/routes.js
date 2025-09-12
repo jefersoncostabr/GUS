172e0aa86a71a76439cf6fca626f28b0f1a83fa9
jefersoncostabr <jefersonb88@gmail.com>
2025-09-11 22:37:56 -0300

    teste usando path

--- routes/routes.js
+++ routes/routes.js
@@ -1,9 +1,10 @@
 import express from 'express';
 import routerUsos from './routerUsos.js';
+import path from 'path';
 
 const router = express.Router();
 
-router.use("/painel", express.static("frontEnd/painelGeral"));// http://localhost:3000/painel/painelGeral.html
+router.use("/painel", express.static(path.join(process.cwd(), "frontEnd/painelGeral")));// http://localhost:3000/painel/painelGeral.html
 router.use("/usos", routerUsos);// http://localhost:3000/usos
 
 export default router;
