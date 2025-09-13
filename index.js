76962a20ca0414a9421c348e48d1ae9795b47fa1
jefersoncostabr <jefersonb88@gmail.com>
2025-09-13 11:02:24 -0300

    dirname implementado

--- index.js
+++ index.js
@@ -2,6 +2,11 @@ import express from 'express';
 import dotenv from 'dotenv';
 import router from './routes/routes.js';
 import conectaNaDatabase from './src/config/dbConnect.js';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const conexao = await conectaNaDatabase();
 
@@ -18,7 +23,8 @@ const app = express();
 app.use(express.json());
 app.use('/', router);
 
-app.use(express.static('frontend/painelgeral'));
+// app.use(express.static('frontend/painelgeral'));
+app.use(express.static(path.join(__dirname, 'frontend/painelgeral')));
 
 const PORT = process.env.PORT || 3000;
 
