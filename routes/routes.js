d13d6c0658daebe4f05b38b9ec353660087a365b
jefersoncostabr <jefersonb88@gmail.com>
2026-01-06 14:50:58 -0300

    confito no routes correção

--- routes/routes.js
+++ routes/routes.js
@@ -9,23 +9,11 @@ router.get("/", (req, res) => {// http://localhost:3000
     res.status(200).send("Rota inicial funcionando");
 });
 
-<<<<<<< HEAD
-router.use(express.static('frontend/adm'));
-router.use(express.static('frontend/painelgeral'));
+router.use(express.static('frontend'));
 // router.use(authMiddleware, express.static('frontend/painelgeral'));
 
-router.use(express.static('frontend/sobre'));
-router.use(express.static('frontend/login'));
-router.use(express.static('frontend/loginCriar'));
 router.use("/usos", routerUsos);// http://localhost:3000/usos
 router.use("/solicitantes", routerSolicitantes);// http://localhost:3000/solicitantes
-=======
-router.use(express.static('frontend'));// http://localhost:3000/painelgeral.html
-// router.use(authMiddleware, express.static('frontend'));
-
-router.use("/usos", routerUsos);// http://localhost:3000/usos+
-router.use("/solicitantes", routerSolicitantes);// http://localhost:3000/solicitantes+
->>>>>>> format-inputs
 
 
 export default router;
